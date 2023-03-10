7ac4261cd06c3d38df89ce36f9a0a36680be00d1
davemysta <46573430+davemysta@users.noreply.github.com>
2023-03-10 06:45:52 -0500

    Add db connection, config folder, db js file, default json file for config

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+//CONNECT DB
+connectDB();
 
 app.get("/", (req, res) => res.send("Main Page"));
 
