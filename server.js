3bee276fd049aaaaa82f2ef3c6300b87032af6a7
davemysta <46573430+davemysta@users.noreply.github.com>
2023-03-11 06:50:08 -0500

    Add routes and routes/api folders, add auth, post, profile, user js files, update server.js to use new routes.

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const connectDB = require("./config/db");
 //CONNECT DB
 connectDB();
 
+//Define Routes
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
 app.get("/", (req, res) => res.send("Main Page"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
